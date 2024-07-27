26736570eb6d56e11838d7254e56a1b55a69e882
malikali001 <malik.alihussain001@gmail.com>
2024-07-27 10:10:27 +0500

    add models and install a new app

--- farm_management_system/settings.py
+++ farm_management_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    'goat_farm',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'farm_management_system.urls'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 TEMPLATES = [
     {
