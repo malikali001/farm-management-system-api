eceb7bd3884e3e1f0dcb30161fd98971dcbcce53
malikali001 <malik.alihussain001@gmail.com>
2024-07-28 12:52:13 +0500

    dockerize the project with postgres database

--- farm_management_system/settings.py
+++ farm_management_system/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = 'farm_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
